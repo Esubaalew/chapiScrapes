9d47c0b9838f8a372ffd6ee72b18fb3a05cdaecf
esubaalew <esubalewchekol6@gmail.com>
2024-12-12 23:43:25 +0300

    made function to get categories

--- scrape.py
+++ scrape.py
@@ -1,28 +1,49 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
 
 def navigate_to_usa_page():
     driver = webdriver.Chrome()
-
     url = 'https://www.bestbuy.com/'
     driver.get(url)
-
-
     if "Choose a country" in driver.page_source:
-        #click the USA country option
         usa_button = driver.find_element(By.CSS_SELECTOR, 'a.us-link')
         usa_button.click()
-
-
     current_url = driver.current_url
     page_source = driver.page_source
     driver.quit()
+    return page_source
 
-    return current_url, page_source
+
+def get_categories_from_page(page_source):
+    soup = BeautifulSoup(page_source, 'html.parser')
+    categories = []
+
+    for item in soup.select('li.c-carousel-item'):
+        # Exclude items containing product-specific details like ratings
+        if item.select_one('.product-rating'):
+            continue
+        
+        category = item.find('a')
+        if category:
+            name = category.get_text(strip=True)
+            url = category['href']
+            
+            if name:
+                categories.append({
+                    'name': name,
+                    'url': url
+                })
+
+    return categories
 
 
-current_url, page_source = navigate_to_usa_page()
 
-print("Current URL:", current_url)
-print(page_source[:500]) 
+
+page_source = navigate_to_usa_page()
+categories = get_categories_from_page(page_source)
+
+print("Categories found:")
+for category in categories:
+    print(category)
